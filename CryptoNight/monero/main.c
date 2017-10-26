a5042f68e8cf73846dc28e00d87a79a530172a99
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-10-26 23:10:47 +0900

    ....

---
+++ CryptoNight/monero/main.c
@@ -0,0 +1,19 @@
+#include "hash-ops.h"
+#include <stdio.h>
+
+int main() {
+    int len = 32;
+    char inputChar[len];
+
+    char h[32];
+
+    cn_slow_hash(&inputChar, len, &h[0]);
+
+    for (int i = 0; i < sizeof(h); i++) {
+        printf("%02x", (unsigned char)h[i]);
+    }
+
+    printf("\n%s", h);
+    printf("unko\n");
+return 0;
+}
